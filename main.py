5dff3b69e6e07b2cd2d4a198bc61ed8799fd4e5a
kennpat <kennedypr@gmail.com>
2014-06-12 19:08:24 -0400

    added drop down, and additional code for get/post.

--- main.py
+++ main.py
@@ -13,9 +13,12 @@ class Main(flask.views.MethodView):
 	def get(self):
 		return flask.render_template('index.html')
 
-	
+	def post(self):
+		selection =  str(flask.request.form['TeamName'])
+		print selection
+		
 
-app.add_url_rule('/', view_func=Main.as_view('main'))		
+app.add_url_rule('/', view_func=Main.as_view('main'), methods=['GET', 'POST'])		
 
 
 app.debug = True
